5f34f6cca229ef0f2297f26fc7ca2978050b013f
wu-yiqiang <wu_yiqiang@outlook.com>
2024-04-18 06:56:55 +0800

    达萨

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
-// import { readFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
-  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate. You retrieve your certificate and key using mkcert.
-  // Learn more:
-  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
-  //
-  // server: {
-  //   port: 443,
-  //   https: {
-  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './https-cert.pem')),
-  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './https-key.pem')),
-  //   },
-  //   host: 'tma.internal',
-  // },
   resolve: {
     alias: {
       '~': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
